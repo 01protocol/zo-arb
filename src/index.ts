ca471dccf90d611037c54af562bce36144456932
Bit Tensor <>
2022-02-08 02:10:01 -0500

    Start logic

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { Bot } from "./Bot";
+import { main } from "./Drift";
 import * as dotenv from "dotenv";
 
 (async () => {
   dotenv.config();
+
+  await main();
 
   const bot = new Bot();
   await bot.run();
